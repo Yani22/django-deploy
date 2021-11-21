045308e78ce1ef55e7e3b42ad538ba5bd25dc98b
Crestian Rex Cabunilas <crestian.rex@gmail.com>
2021-11-21 18:25:47 +0800

    Update settings.py

--- dirsys/settings.py
+++ dirsys/settings.py
@@ -134,7 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "/home/yanicabs/yanicabs.pythonanywhere.com/media_cdn/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media_cdn")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
